0236c67d6fa0695e4c2fcdf8958736319dc084db
yaxyobekuz <pubgn9642@gmail.com>
2026-03-18 14:21:32 +0500

    refactor: remove unused API files for classes, grades, holidays, messages, penalties, schedules, subjects, topics, and users

--- src/shared/api/subjects.api.js
+++
@@ -1,9 +0,0 @@
-import http from "@/shared/api/http";
-
-export const subjectsAPI = {
-  getAll: () => http.get("/subjects"),
-  create: (data) => http.post("/subjects", data),
-  update: (id, data) => http.put(`/subjects/${id}`, data),
-  delete: (id) => http.delete(`/subjects/${id}`),
-  export: () => http.get("/subjects/export", { responseType: "blob" }),
-};
