62fa0bb8c3c38a86667dc1701d15042223ee995f
yaxyobekuz <pubgn9642@gmail.com>
2026-03-18 14:23:11 +0500

    feat: update API import paths in DashboardLayout component

--- src/shared/layouts/DashboardLayout.jsx
+++ src/shared/layouts/DashboardLayout.jsx
@@ -5,10 +5,10 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 // API
-import { usersAPI } from "@/shared/api/users.api";
-import { classesAPI } from "@/shared/api/classes.api";
-import { holidaysAPI } from "@/shared/api/holidays.api";
-import { subjectsAPI } from "@/shared/api/subjects.api";
+import { usersAPI } from "@/features/users/api/users.api";
+import { classesAPI } from "@/features/classes/api/classes.api";
+import { holidaysAPI } from "@/features/holidays/api/holidays.api";
+import { subjectsAPI } from "@/features/subjects/api/subjects.api";
 
 // Hooks
 import useAuth from "@/shared/hooks/useAuth";
