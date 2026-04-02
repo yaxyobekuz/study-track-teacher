7e628918fcb3fca31f72275e43ea1b07e77758ec
yaxyobekuz <pubgn9642@gmail.com>
2026-04-02 22:03:12 +0500

    refactor: organize imports and improve code formatting in Input component

--- src/shared/components/shadcn/input.jsx
+++ src/shared/components/shadcn/input.jsx
@@ -1,19 +1,23 @@
-import * as React from "react"
+// React
+import * as React from "react";
 
-import { cn } from "@/shared/utils/cn"
+// Utils
+import { cn } from "@/shared/utils/cn.js";
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     <input
+      ref={ref}
       type={type}
       className={cn(
         "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
+        className,
       )}
-      ref={ref}
-      {...props} />
+      {...props}
+    />
   );
-})
-Input.displayName = "Input"
+});
 
-export { Input }
+Input.displayName = "Input";
+
+export { Input };
