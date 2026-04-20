280cb5f4b73ec58a5c45965626265a543f80c877
yaxyobekuz <pubgn9642@gmail.com>
2026-04-20 21:13:52 +0500

    refactor: enhance LoginPage layout and structure, update form components and animations

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -1,26 +1,68 @@
 // Toast
 import { toast } from "sonner";
 
-// Images
-import { logo } from "@/shared/assets/images";
+// Lottie
+import Lottie from "lottie-react";
+
+// Utils
+import { cn } from "@/shared/utils/cn";
 
 // Router
 import { useNavigate } from "react-router-dom";
 
+// Icons
+import { logoIcon } from "@/shared/assets/icons";
+
 // API
 import { authAPI } from "@/features/auth/api/auth.api";
-
-// Components
-import Card from "@/shared/components/ui/Card";
-import { Button } from "@/shared/components/shadcn/button";
 
 // Hooks
 import useObjectState from "@/shared/hooks/useObjectState";
 
+// Animations
+import { lockWithKeyEmojiAnimation } from "@/shared/assets/animations";
+
+// Components
+import Button from "@/shared/components/ui/button/Button";
+import InputGroup from "@/shared/components/ui/input/InputGroup";
+import InputField from "@/shared/components/ui/input/InputField";
+import MainBackgroundPatterns from "@/shared/components/bg/MainBackgroundPatterns";
+
 const LoginPage = () => {
+  return (
+    <div className="flex flex-row-reverse w-full h-svh">
+      {/* Form */}
+      <div
+        className={cn(
+          "flex items-center justify-center size-full relative z-10 bg-white/50 backdrop-blur px-5 transition-transform duration-500 md:w-1/2",
+        )}
+      >
+        <LoginForm />
+      </div>
+
+      {/* Animation Data */}
+      <div
+        className={cn(
+          "hidden items-center justify-center w-1/2 h-full transition-transform duration-500 md:flex",
+        )}
+      >
+        <Lottie
+          animationData={lockWithKeyEmojiAnimation}
+          className="size-64 animate__animated animate__fadeIn"
+        />
+      </div>
+
+      {/* Background Patterns */}
+      <MainBackgroundPatterns />
+    </div>
+  );
+};
+
+const LoginForm = ({}) => {
   const navigate = useNavigate();
 
   const { username, password, setField, isLoading } = useObjectState({
+    step: 1,
     username: "",
     password: "",
     isLoading: false,
@@ -35,9 +77,11 @@ const LoginPage = () => {
     authAPI
       .login(data)
       .then((response) => {
+        // Save token to localStorage
         const { token } = response.data.data;
         localStorage.setItem("authToken", token);
-        toast.success("Tizimga muvaffaqiyatli kirdingiz!");
+
+        // Navigate to dashboard
         navigate("/dashboard");
       })
       .catch((error) => {
@@ -49,64 +93,57 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-100 to-white px-4">
-      <div className="max-w-md w-full">
-        <Card className="p-8 border-none">
-          {/* Header */}
-          <div className="text-center mb-8">
-            <img
-              width={64}
-              alt="Logo"
-              height={64}
-              src={logo}
-              className="size-16 mx-auto mb-4"
-            />
-            <h2 className="text-3xl font-bold text-gray-900">MBSI School</h2>
-            <p className="text-gray-600 mt-2">Tizimga kirish</p>
-          </div>
+    <div className="max-w-md w-full animate__animated animate__fadeIn">
+      {/* Header */}
+      <div className="text-center mb-8 space-y-3.5">
+        {/* Title */}
+        <h2 className="flex items-center justify-center gap-3.5 text-lg font-medium text-center md:gap-5 md:text-xl">
+          <img
+            width={32}
+            height={32}
+            src={logoIcon}
+            className="size-8"
+            alt="MBSI Logo icon"
+          />
 
-          {/* Form */}
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="space-y-1.5">
-              <label htmlFor="username" className="text-sm text-gray-600">
-                Username
-              </label>
-              <input
-                required
-                id="username"
-                name="username"
-                value={username}
-                autoComplete="username"
-                className="w-full h-11 px-3 rounded-lg border border-gray-300 bg-white text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                onChange={(event) =>
-                  setField("username", event.target.value.trim().toLowerCase())
-                }
-              />
-            </div>
+          <span>Qaytganingiz bilan!</span>
+        </h2>
 
-            <div className="space-y-1.5">
-              <label htmlFor="password" className="text-sm text-gray-600">
-                Parol
-              </label>
-              <input
-                required
-                minLength={6}
-                id="password"
-                name="password"
-                type="password"
-                value={password}
-                autoComplete="current-password"
-                className="w-full h-11 px-3 rounded-lg border border-gray-300 bg-white text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                onChange={(event) => setField("password", event.target.value)}
-              />
-            </div>
-
-            <Button type="submit" disabled={isLoading} className="w-full">
-              Kirish{isLoading && "..."}
-            </Button>
-          </form>
-        </Card>
+        {/* Description */}
+        <p className="text-gray-600 mt-2">
+          Tizimga kirish uchun ma'lumotlaringizni kiriting.
+        </p>
       </div>
+
+      {/* Form */}
+      <InputGroup as="form" onSubmit={handleSubmit}>
+        <InputField
+          required
+          id="username"
+          name="username"
+          value={username}
+          autoComplete="username"
+          label="Foydalanuvchi nomi"
+          placeholder="Faqat raqamlar va harflar"
+          onChange={(e) =>
+            setField("username", e.target.value.trim().toLowerCase())
+          }
+        />
+
+        <InputField
+          required
+          id="password"
+          label="O'ron"
+          type="password"
+          name="password"
+          value={password}
+          autoComplete="current-password"
+          onChange={(e) => setField("password", e.target.value.trim())}
+        />
+
+        {/* Action buttons */}
+        <Button disabled={isLoading}>Tizimga kirish{isLoading && "..."}</Button>
+      </InputGroup>
     </div>
   );
 };
